1e01dbe1e31d41dc6945542e1502dab3e443cf7e
Илья Нечаев <dolphins.mind777@gmail.com>
2022-03-09 21:28:24 +0300

    Build inteface

---
+++ Cocktail Map/CocktailTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  CocktailTableViewCell.swift
+//  Cocktail Map
+//
+//  Created by Илья Нечаев on 09.03.2022.
+//
+
+import UIKit
+
+class CocktailTableViewCell: UITableViewCell {
+    @IBOutlet weak var cocktailImage: UIImageView!
+    @IBOutlet weak var cocktailNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func configure(with cocktail: Drink) {
+        cocktailNameLabel.text = cocktail.strDrink
+        
+        DispatchQueue.global().async {
+            guard let stringURL = cocktail.strDrinkThumb else { return }
+            guard let imageURL = URL(string: stringURL) else { return }
+            guard let imageData = try? Data(contentsOf: imageURL) else { return }
+            
+            DispatchQueue.main.async {
+                self.cocktailImage.image = UIImage(data: imageData)
+            }
+        }
+    }
+}
